b6fa778781cdbabb15e502c0e0da609b7a3b0eb4
Dinar699 <d.siraziev88@gmail.com>
2023-02-20 15:10:02 +0300

    2 commit

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -5,12 +5,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
-import web.service.CarServiceImpl;
 
 
 @Controller
 public class CarController {
-    private CarService carService = new CarServiceImpl();
+    private final CarService carService;
+
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
 
     @GetMapping("/cars")
     public String getCars(@RequestParam(value = "count", defaultValue = "5") int count, Model model ) {
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,24 +1,40 @@
 package web.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import web.dao.CarDao;
 import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@Service
 public class CarServiceImpl implements CarService{
-    private List<Car> carList = new ArrayList<>() {
-        {
-            add (new Car(1, "auto1", "black"));
-            add (new Car(2, "auto2", "white"));
-            add (new Car(3, "auto3", "blue"));
-            add (new Car(4, "auto4", "yellow"));
-            add (new Car(5, "auto5", "grey"));
-
-        }
-    };
+    @Autowired
+    private CarDao carDao;
+    @Autowired
+    public CarServiceImpl(CarDao carDao) {
+        this.carDao = carDao;
+    }
 
     @Override
     public List<Car> getCarList(int count) {
-        return carList.stream().limit(count).toList();
+        return carDao.getCarList(count);
     }
+
+
+//    private List<Car> carList = new ArrayList<>() {
+//        {
+//            add (new Car(1, "auto1", "black"));
+//            add (new Car(2, "auto2", "white"));
+//            add (new Car(3, "auto3", "blue"));
+//            add (new Car(4, "auto4", "yellow"));
+//            add (new Car(5, "auto5", "grey"));
+//
+//        }
+//    };
+//
+//    @Override
+//    public List<Car> getCarList(int count) {
+//        return carList.stream().limit(count).toList();
+//    }
 }
