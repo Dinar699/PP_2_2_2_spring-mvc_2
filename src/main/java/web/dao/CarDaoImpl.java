eb3732ec68f91b24bd31f3eb50b40ecbcf16175b
Dinar699 <d.siraziev88@gmail.com>
2023-02-20 15:13:49 +0300

    release v2

---
+++ src/main/java/web/dao/CarDaoImpl.java
@@ -0,0 +1,27 @@
+package web.dao;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+@Component
+public class CarDaoImpl implements CarDao{
+    private List<Car> carList = new ArrayList<>() {
+        {
+            add (new Car(1, "auto1", "black"));
+            add (new Car(2, "auto2", "white"));
+            add (new Car(3, "auto3", "blue"));
+            add (new Car(4, "auto4", "yellow"));
+            add (new Car(5, "auto5", "grey"));
+
+        }
+    };
+
+    @Override
+    public List<Car> getCarList(int count) {
+        return carList.stream().limit(count).toList();
+
+    }
+}
